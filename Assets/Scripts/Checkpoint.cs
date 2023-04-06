81b46edcd17de0d29e67592d858893646bad8ae2
codywashbrook <codywashbrook@hotmail.com>
2023-04-06 01:17:50 -0300

    still fixing fixing checkpoints and killbox

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Checkpoint : MonoBehaviour
 {
     Vector3 spawnPoint;
+    [SerializeField] private Transform player;
     // Start is called before the first frame update
     void Start()
     {
